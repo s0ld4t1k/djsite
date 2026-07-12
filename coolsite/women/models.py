6cea4533a19c5220a98bc1cda9866053a9c58087
s0ld4t1k <begnazarowbegnazar11@gmail.com>
2026-07-12 14:22:03 +0500

    AdminPanel

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.urls import reverse
 
 class Women(models.Model):
-    title=models.CharField(max_length=255)
-    content=models.TextField(blank=True)
-    photo=models.ImageField(upload_to="photos/%Y/%m/%d")
-    time_create=models.DateTimeField(auto_now_add=True)
-    time_update=models.DateTimeField(auto_now=True)
-    is_published=models.BooleanField(default=True)
-    cat=models.ForeignKey('Category',on_delete=models.PROTECT,null=True)
+    title=models.CharField(max_length=255,verbose_name='Ady')
+    content=models.TextField(blank=True,verbose_name='Tekst')
+    photo=models.ImageField(upload_to="photos/%Y/%m/%d",verbose_name='Surat')
+    time_create=models.DateTimeField(auto_now_add=True,verbose_name='Doran wagty')
+    time_update=models.DateTimeField(auto_now=True,verbose_name='Uytgan wagty')
+    is_published=models.BooleanField(default=True,verbose_name='Publikasiya')
+    cat=models.ForeignKey('Category',on_delete=models.PROTECT,null=True,verbose_name='Kategoriya')
 
     def __str__(self):
         return self.title
@@ -16,11 +16,21 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post',kwargs={'post_id':self.id})
 
+    class Meta:
+        verbose_name='Pornstar'
+        verbose_name_plural='PornStars'
+        ordering=['-time_create','title']
+
 class Category(models.Model):
-    name=models.CharField(max_length=100,db_index=True)
+    name=models.CharField(max_length=100,db_index=True,verbose_name='Ady')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category',kwargs={'cat_id':self.id})
+
+    class Meta:
+        verbose_name='Kategoriya'
+        verbose_name_plural='Kategoriyalar'
+        ordering=['id']
