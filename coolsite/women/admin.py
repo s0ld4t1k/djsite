ed02e6ae626a6a44b1337212c624ea2a510ddb36
s0ld4t1k <begnazarowbegnazar11@gmail.com>
2026-07-26 13:21:03 +0500

    AdminPanel templates

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from .models import *
+from django.utils.safestring import mark_safe
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display=['id','title','time_create','photo','is_published']
+    list_display=['id','title','time_create','get_html_photo','is_published']
     list_display_links=['id','title']
     search_fields=['title','content']
     list_editable=['is_published']
     list_filter=['is_published','time_create']
     prepopulated_fields={'slug':['title']}
+    fields=['title','slug','cat','content','photo','get_html_photo','is_published','time_create','time_update']
+    readonly_fields=['time_create','time_update','get_html_photo']
+    save_on_top=True
 
+    def get_html_photo(self,object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+    get_html_photo.short_description='Surat'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display=['id','name',]
@@ -18,3 +26,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Women,WomenAdmin)
 admin.site.register(Category,CategoryAdmin)
+
+admin.site.site_title='AdminPanel of PornStars'
+admin.site.site_header='AdminPanel of bitches'
